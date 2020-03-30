2275ed0349b3b5356f1ef9dde963afb5179179df
Arkaywhon <ryokinjo@cmail.carleton.ca>
2020-03-30 13:14:21 -0400

    Tidied up mainwindow.cpp and mainwindow.h

--- MyGUI/mainwindow.cpp
+++ MyGUI/mainwindow.cpp
@@ -1,14 +1,4 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include <QtBluetooth>
-#include <QDebug>
-#include "children.h"
-#include "frequency.h"
-#include "med.h"
-#include "programs.h"
-#include "screening.h"
-#include "settings.h"
-
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,39 +6,45 @@ MainWindow::MainWindow(QWidget *parent)
 {
 
     battery = new Battery();
-
-    // this is the ui setup
-    ui->setupUi(this);
-    ui->listWidget->hide();
-    connect(disc, SIGNAL(deviceDiscovered(QBluetoothDeviceInfo)), this, SLOT(deviceDiscovered(QBluetoothDeviceInfo)));
-    disc->start();
-
-    socket = new QBluetoothSocket(QBluetoothServiceInfo::RfcommProtocol);
-
-    // timer setup
     batteryTimer = new QTimer(this);
     connect(batteryTimer, SIGNAL(timeout()), this, SLOT(on_batteryTimer_activated()));
     batteryTimer->start(1000);
 
-    // testing hide property
-    //ui->testButton->hide();
+    ui->setupUi(this);
     setFixedSize(349, 663);
 
-    // trying to add button to widget
-    //QPushButton *btn = new QPushButton("Tes Button", ui->widget);
+    ui->listWidget->hide();
+    ui->list_2->hide();
+    ui->list_3->hide();
+
     counter = -1;
-    //ui->list->
+    mainListSetup();
+
+    // bluetooth related setup
+    connect(disc, SIGNAL(deviceDiscovered(QBluetoothDeviceInfo)), this, SLOT(deviceDiscovered(QBluetoothDeviceInfo)));
+    disc->start();
+    socket = new QBluetoothSocket(QBluetoothServiceInfo::RfcommProtocol);
+  }
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::mainListSetup(){
 
     QListWidgetItem *program = new QListWidgetItem;
       program->setText("Programs");
       program->setTextAlignment(Qt::AlignHCenter);
       program->setSizeHint(QSize(0, 35));
+      m[program]=ui->list_2;
       ui->list->insertItem(0, program);
 
     QListWidgetItem *frequency = new QListWidgetItem;
       frequency->setText("Frequency");
       frequency->setTextAlignment(Qt::AlignHCenter);
       frequency->setSizeHint(QSize(0, 35));
+      m[frequency]=ui->list_3;
       ui->list->insertItem(1, frequency);
 
     QListWidgetItem *med = new QListWidgetItem;
@@ -74,12 +70,6 @@ MainWindow::MainWindow(QWidget *parent)
       settings->setTextAlignment(Qt::AlignHCenter);
       settings->setSizeHint(QSize(0, 35));
       ui->list->insertItem(2, settings);
-    //ui->list->addItem()
-  }
-
-MainWindow::~MainWindow()
-{
-    delete ui;
 }
 
 void MainWindow::on_batteryTimer_activated(){
@@ -95,51 +85,36 @@ void MainWindow::on_Find_Device_clicked()
     disc->start();
 }
 
-void MainWindow::on_Bluetooth_On_clicked()
-{
-    socket->write("ON");
-}
+void MainWindow::on_Bluetooth_On_clicked() {socket->write("ON");}
 
-void MainWindow::on_Bluetoot_Off_clicked()
-{
-    socket->write("OFF");
-}
+void MainWindow::on_Bluetoot_Off_clicked() {socket->write("OFF");}
 
-void MainWindow::deviceDiscovered(const QBluetoothDeviceInfo &device)
-{
+void MainWindow::deviceDiscovered(const QBluetoothDeviceInfo &device){
     ui->listWidget->addItem(device.address().toString());
     ui->listWidget->setCurrentRow(1);
 }
 
-void MainWindow::on_listWidget_itemClicked(QListWidgetItem *item)
-{
+void MainWindow::on_listWidget_itemClicked(QListWidgetItem *item){
     string = item->text();
     ui->listWidget->addItem(string);
     static const QString serviceUuid(QStringLiteral("00001101-0000-1000-8000-00805F9B34FB"));
     socket->connectToService(QBluetoothAddress(string), QBluetoothUuid(serviceUuid), QIODevice::ReadWrite);
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-//    if (ui->label->text() == "Status: ON"){
-//        ui->label->setText("Status: OFF");
-//    }else {
-//       ui->label->setText("Status: ON");
-//    }
+void MainWindow::on_pushButton_clicked(){
+    if(m.find(ui->listWidget->currentItem()) == m.end()){
+        qDebug() << "Not found";
+    }else{
+        m[ui->listWidget->currentItem()]->show();
+    }
 }
 
-void MainWindow::on_pushButton_3_clicked()
-{
-    if (!(counter > ui->list->count() - 2)){
-        counter ++;
-    }
+void MainWindow::on_pushButton_3_clicked(){
+    if (!(counter > ui->list->count() - 2)) counter ++;
     ui->list->setCurrentItem(ui->list->item(counter));
 }
 
-void MainWindow::on_pushButton_2_clicked()
-{
-    if (counter > 0){
-        counter --;
-    }
+void MainWindow::on_pushButton_2_clicked(){
+    if (counter > 0) counter --;
     ui->list->setCurrentItem(ui->list->item(counter));
 }
--- MyGUI/mainwindow.h
+++ MyGUI/mainwindow.h
@@ -5,7 +5,21 @@
 #include <QBluetoothDeviceDiscoveryAgent>
 #include <QBluetoothSocket>
 #include <QListWidgetItem>
+#include <QtBluetooth>
+#include <QDebug>
+
+#include <map>
+
 #include "battery.h"
+#include "ui_mainwindow.h"
+#include "children.h"
+#include "frequency.h"
+#include "med.h"
+#include "programs.h"
+#include "screening.h"
+#include "settings.h"
+
+using namespace std;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -20,33 +34,27 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_Find_Device_clicked();
-
-    void on_Bluetooth_On_clicked();
-
-    void on_Bluetoot_Off_clicked();
-
-    void deviceDiscovered(const QBluetoothDeviceInfo &device);
-
     void on_listWidget_itemClicked(QListWidgetItem *item);
-
     void on_pushButton_clicked();
-
     void on_batteryTimer_activated();
-
     void on_pushButton_3_clicked();
-
     void on_pushButton_2_clicked();
+    // bluetooth related slots
+    void on_Find_Device_clicked();
+    void on_Bluetooth_On_clicked();
+    void on_Bluetoot_Off_clicked();
+    void deviceDiscovered(const QBluetoothDeviceInfo &device);
 
 private:
     Ui::MainWindow *ui;
+    map<QListWidgetItem*,QListWidget*> m;
     QBluetoothDeviceDiscoveryAgent *disc = new QBluetoothDeviceDiscoveryAgent;
     QBluetoothSocket *socket;
     QString string;
     QTimer *batteryTimer;
     int counter;
     Battery *battery;
+
+    void mainListSetup();
 };
 #endif // MAINWINDOW_H
-
-//testing 123
